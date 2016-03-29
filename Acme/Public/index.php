478bcc4da63f3f8d9ce428e18517bbd1e04260d5
Lucas Isabel <lucky.o.isa@gmail.com>
2016-03-29 09:31:16 -0300

    Adicionado arquivo Jquery ao Projeto e adicionado no arquivo index

--- Acme/Public/index.php
+++ Acme/Public/index.php
@@ -2,6 +2,26 @@
 
     require "config.php";
 
-    $con = new \Acme\Models\Conexao();
-    echo ($con->getCon()) ? "Conectado" : "Não Conectado";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Project Teste</title>
+    <link rel="stylesheet" href="assets/css/materialize.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+    </div>
+
+</body>
+
+    <script src="assets/js/materialize.min.js"></script>
+    <script src="assets/js/jquery.js"></script>
+
+</html>
 
